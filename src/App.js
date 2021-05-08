7a2411781bad9042befa85182c0ff52157e2e313
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-08 15:18:23 -0700

    Added theme toggle functionality
    - Created theme state variable to keep track of the current theme selected
    - Added useEffect to change the class of the root html element when the theme state changes.
    - Added toggleTheme to toggle theme
    - Add onClick to toggle button to call toggleTheme

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import data from './data';
 import Article from './Article';
 
 function App () {
+	const [ theme, setTheme ] = useState('light-theme');
+
+	const toggleTheme = () => {
+		setTheme(theme === 'light-theme' ? 'dark-theme' : 'light-theme');
+	};
+
+	useEffect(
+		() => {
+			document.documentElement.className = theme;
+		},
+		[ theme ]
+	);
 	return (
 		<main>
 			<nav>
 				<div className='nav-center'>
 					<h1>overreacted</h1>
-					<button className='btn'>toggle</button>
+					<button className='btn' onClick={toggleTheme}>
+						toggle
+					</button>
 				</div>
 			</nav>
 			<section className='articles'>
