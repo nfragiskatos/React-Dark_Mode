926efdd23a560589e7e453f63504853217df0275
root <root@NFrag-Home-Desktop.localdomain>
2021-05-08 15:25:00 -0700

    Updates

---
+++ precache-manifest.af0ee53c800ad9fad22b69d5e4dfa86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4946a1dfa333c9cf1ee381da69df21b3",
+    "url": "/React-Dark_Mode/index.html"
+  },
+  {
+    "revision": "787eaf0663428de1d9d4",
+    "url": "/React-Dark_Mode/static/css/main.d2afdc71.chunk.css"
+  },
+  {
+    "revision": "4646610705680ce7d806",
+    "url": "/React-Dark_Mode/static/js/2.a99e8b18.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/React-Dark_Mode/static/js/2.a99e8b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787eaf0663428de1d9d4",
+    "url": "/React-Dark_Mode/static/js/main.ff21a8f0.chunk.js"
+  },
+  {
+    "revision": "c8892fd60aea25f87317",
+    "url": "/React-Dark_Mode/static/js/runtime-main.7e0611a3.js"
+  }
+]);
